3787e71e1a1e9c6387e1d24ca36234a3acecfede
Rapcis <Rapcis@users.noreply.github.com>
2018-11-21 22:29:35 +0000

    Updates

---
+++ precache-manifest.9f54162cfabbe0f03fafe03490e5dc4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2cc5415049c106418e4e26275e5b794",
+    "url": "/robofriends/static/media/cubicblock_b.b2cc5415.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "576f6c5a539bf440d554",
+    "url": "/robofriends/static/js/main.576f6c5a.chunk.js"
+  },
+  {
+    "revision": "55972ff598234a29e34e",
+    "url": "/robofriends/static/js/1.55972ff5.chunk.js"
+  },
+  {
+    "revision": "576f6c5a539bf440d554",
+    "url": "/robofriends/static/css/main.d49be6e6.chunk.css"
+  },
+  {
+    "revision": "55972ff598234a29e34e",
+    "url": "/robofriends/static/css/1.e2326b97.chunk.css"
+  },
+  {
+    "revision": "0de1c8bcabea2e89538206acc389a66d",
+    "url": "/robofriends/index.html"
+  }
+];
