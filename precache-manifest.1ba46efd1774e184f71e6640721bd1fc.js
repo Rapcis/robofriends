94f6a1cc4df5205c71505f8c4809041291973cca
Rapcis <Rapcis@users.noreply.github.com>
2018-11-21 22:08:03 +0000

    Updates

---
+++ precache-manifest.1ba46efd1774e184f71e6640721bd1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2cc5415049c106418e4e26275e5b794",
+    "url": "/static/media/cubicblock_b.b2cc5415.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "355d8a8cc08cc0e6292c",
+    "url": "/static/js/main.355d8a8c.chunk.js"
+  },
+  {
+    "revision": "55972ff598234a29e34e",
+    "url": "/static/js/1.55972ff5.chunk.js"
+  },
+  {
+    "revision": "355d8a8cc08cc0e6292c",
+    "url": "/static/css/main.d00b1f88.chunk.css"
+  },
+  {
+    "revision": "55972ff598234a29e34e",
+    "url": "/static/css/1.e2326b97.chunk.css"
+  },
+  {
+    "revision": "7ddd994a6b7db9c95e8a068dfe5b83d1",
+    "url": "/index.html"
+  }
+];
